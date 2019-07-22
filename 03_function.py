ab54b76952a946697325a75f258fb2d6ef3b1177
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-22 13:30:18 +0100

    Update 03_function.py

--- 03_function.py
+++ 03_function.py
@@ -92,3 +92,31 @@ parrot(voltage=1000000, action='VOOOOOM') # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000) # 2 keyword arguments 
 parrot('a million', 'bereft of life', 'jump') # 3 positional arguments 
 parrot('a thousand', state='pushing up the daisies') # 1 positional, 1 keywor
+
+
+"""
+When a final formal parametter of form **name is present, it receives a dictionary contain all the keyword arguments except for those corresponging to 
+a formal parameter
+
+This maybe combined with a formal parameter of the form *name which receives a tuple contain the postional arguments byond 
+the formal parameter list 
+
+(*name must occur before **name)
+
+"""
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any ", kind,  "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+     print(arg)
+    for kw in keywords:
+     print (kw, ":", keywords[kw])
+
+cheeseshop(
+    "Limburger", 
+    "It's very runny, sir.", 
+    "It's really very, VERY runny, sir.", 
+    shopkeeper="Michael Palin", 
+    client="John Cleese", 
+    sketch="Cheese Shop Sketch")
+
