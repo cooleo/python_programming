179c8321cf019014330f436cc39d41c6ac6e040b
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-28 13:55:14 +0100

    update@

--- 01_data_structure/Dictionary.py
+++ 01_data_structure/Dictionary.py
@@ -8,3 +8,7 @@ array_of_dic = [{'name':'Hung'}, {'name':'Kul'}, {'name':'Lien'}]
 print(array_of_dic)
 
 print(array_of_dic[0]['name'])
+
+array_of_dic.append({'name':'Vietnam'})
+
+print(array_of_dic)
