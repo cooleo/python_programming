f025fbd9fa550436c3632566c2b0150ab9ade8c3
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-07-29 11:52:51 +0100

    Update 06_string.py

--- 06_string.py
+++ 06_string.py
@@ -1,2 +1,8 @@
 print('{0}, {1}, {2}'format('a', 'b', 'c'))
 print('{2}, {1}, {0}'.format(*'abc'))
+print('hello world'.islower())
+print('138383'.isnumeric())
+print('1938383'.isdigit())
+print('8383.8383883'.isdecimal())
+print('hello world'.ispace())
+
